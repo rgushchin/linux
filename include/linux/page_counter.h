ecd02ab3547b49cac81913b1aedd746c7013d4e4
Roman Gushchin <roman.gushchin@linux.dev>
2024-07-16 01:50:30 +0000

    mm: page_counters: put page_counter_calculate_protection() under CONFIG_MEMCG
    Put page_counter_calculate_protection() under CONFIG_MEMCG.
    
    The protection functionality (min/low limits) is not supported by any
    other cgroup subsystem, so page_counter_calculate_protection() and
    related static effective_protection() can be compiled out if
    CONFIG_MEMCG is not enabled.
    
    Signed-off-by: Roman Gushchin <roman.gushchin@linux.dev>

--- include/linux/page_counter.h
+++ include/linux/page_counter.h
@@ -91,8 +91,14 @@ static inline void page_counter_reset_watermark(struct page_counter *counter)
 	counter->local_watermark = usage;
 }
 
+#ifdef CONFIG_MEMCG
 void page_counter_calculate_protection(struct page_counter *root,
 				       struct page_counter *counter,
 				       bool recursive_protection);
+#else
+static inline void page_counter_calculate_protection(struct page_counter *root,
+						     struct page_counter *counter,
+						     bool recursive_protection) {}
+#endif
 
 #endif /* _LINUX_PAGE_COUNTER_H */
