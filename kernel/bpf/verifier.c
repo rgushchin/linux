4c6e691dc422cb2ea1ab90c0731c0c9a17fb1a58
Roman Gushchin <roman.gushchin@linux.dev>
2025-08-11 18:05:43 -0700

    bpf: mark struct oom_control's memcg field as TRUSTED_OR_NULL
    Struct oom_control is used to describe the OOM context.
    It's memcg field defines the scope of OOM: it's NULL for global
    OOMs and a valid memcg pointer for memcg-scoped OOMs.
    Teach bpf verifier to recognize it as trusted or NULL pointer.
    It will provide the bpf OOM handler a trusted memcg pointer,
    which for example is required for iterating the memcg's subtree.
    
    Signed-off-by: Roman Gushchin <roman.gushchin@linux.dev>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -7035,6 +7035,10 @@ BTF_TYPE_SAFE_TRUSTED_OR_NULL(struct socket) {
 	struct sock *sk;
 };
 
+BTF_TYPE_SAFE_TRUSTED_OR_NULL(struct oom_control) {
+	struct mem_cgroup *memcg;
+};
+
 static bool type_is_rcu(struct bpf_verifier_env *env,
 			struct bpf_reg_state *reg,
 			const char *field_name, u32 btf_id)
@@ -7075,6 +7079,7 @@ static bool type_is_trusted_or_null(struct bpf_verifier_env *env,
 {
 	BTF_TYPE_EMIT(BTF_TYPE_SAFE_TRUSTED_OR_NULL(struct socket));
 	BTF_TYPE_EMIT(BTF_TYPE_SAFE_TRUSTED_OR_NULL(struct dentry));
+	BTF_TYPE_EMIT(BTF_TYPE_SAFE_TRUSTED_OR_NULL(struct oom_control));
 
 	return btf_nested_type_is_trusted(&env->log, reg, field_name, btf_id,
 					  "__safe_trusted_or_null");
